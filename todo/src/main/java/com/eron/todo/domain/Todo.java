70038ae447d5d58e89a0d375e85d9700dc8fbb68
Eron Moraes <eron.moraes7@gmail.com>
2023-07-05 22:26:37 -0300

    teste

--- todo/src/main/java/com/eron/todo/domain/Todo.java
+++ todo/src/main/java/com/eron/todo/domain/Todo.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+//Teste
 
 @Entity
 public class Todo implements Serializable {
