05bd9547e04796950957b8b8da366ad77f918488
Eron Moraes <eron.moraes7@gmail.com>
2023-07-06 23:41:55 -0300

    commit4

--- todo/src/main/java/com/eron/todo/TodoApplication.java
+++ todo/src/main/java/com/eron/todo/TodoApplication.java
@@ -1,13 +1,20 @@
 package com.eron.todo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
