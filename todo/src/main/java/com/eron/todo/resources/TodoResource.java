26ad07ef3ce21d4e7ecf097bb59223e86caf753c
Eron Moraes <eron.moraes7@gmail.com>
2023-07-11 23:19:10 -0300

    build commit10

--- todo/src/main/java/com/eron/todo/resources/TodoResource.java
+++ todo/src/main/java/com/eron/todo/resources/TodoResource.java
@@ -1,13 +1,17 @@
 package com.eron.todo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eron.todo.domain.Todo;
 import com.eron.todo.services.TodoService;
@@ -15,36 +19,43 @@ import com.eron.todo.services.TodoService;
 @RestController
 @RequestMapping(value = "/todos")
 public class TodoResource {
-	
+
 	@Autowired
 	private TodoService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Todo> findById(@PathVariable Integer id) {
 		Todo obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-		
+
 	}
+
 	@GetMapping(value = "/open")
-	public ResponseEntity<List<Todo>> listOpen(){
+	public ResponseEntity<List<Todo>> listOpen() {
 		List<Todo> list = service.findAllOpen();
 		return ResponseEntity.ok().body(list);
-		
+
 	}
-	
+
 	@GetMapping(value = "/close")
-	public ResponseEntity<List<Todo>> listClose(){
+	public ResponseEntity<List<Todo>> listClose() {
 		List<Todo> list = service.findAllClose();
 		return ResponseEntity.ok().body(list);
-		
+
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<Todo>> listAll(){
+	public ResponseEntity<List<Todo>> listAll() {
 		List<Todo> list = service.findAll();
 		return ResponseEntity.ok().body(list);
-		
-		
+
 	}
 	
+	@PostMapping
+	public ResponseEntity<Todo> create(@RequestBody Todo obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- todo/src/main/java/com/eron/todo/services/TodoService.java
+++ todo/src/main/java/com/eron/todo/services/TodoService.java
@@ -35,4 +35,9 @@ public class TodoService {
 		return list;
 	}
 
+	public Todo create(Todo obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
 }
